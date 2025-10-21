255e1b9a3cd738a16c0b2c05ba7212d053bf2374
Emmycchhuu <accbombb@gmail.com>
2025-10-21 21:00:38 +0100

    Add VoiceVerificationPage component

---
+++ components/VoiceVerification.jsx
@@ -0,0 +1,7 @@
+"use client"
+
+import VoiceVerificationClient from "@/components/VoiceVerificationClient"
+
+export default function VoiceVerificationPage() {
+  return <VoiceVerificationClient />
+}
